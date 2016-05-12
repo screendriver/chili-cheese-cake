6d6e02e08065fb607682bb21436ecfe2130d1328
Christian Rackerseder <git@echooff.de>
2016-05-12 21:17:07 +0200

    First hello world code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import 'babel-polyfill';
+
+import Botkit from 'botkit';
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+const controller = Botkit.slackbot({
+  debug: true,
+});
+
+controller.spawn({
+  token: process.env.token,
+}).startRTM();
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', (bot, message) => {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  }, err => {
+    if (err) {
+      bot.botkit.log('Failed to add emoji reaction :(', err);
+    }
+  });
+});
